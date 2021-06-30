8dbd69896523a1580823e1012a14f27d1e41c78c
Delvin <delvinfernandezm@gmail.com>
2021-06-30 12:03:59 -0500

    primer  node

---
+++ backend/data.js
@@ -0,0 +1,81 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '2',
+            name: 'Adidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 3,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 120,
+            countInStock: 0,
+            brand: 'Lacoste',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 120,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '5',
+            name: 'Puma Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 79,
+            countInStock: 5,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '6',
+            name: 'Adidas Fit Pant',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 120,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'high quality product',
+        },
+
+    ]
+};
+
+
+
+export default data;
