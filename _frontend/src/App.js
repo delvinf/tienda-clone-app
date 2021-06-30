15dfb70ff1a2a61b8bf6b412a0c3f9f9028fe660
Delvin <delvinfernandezm@gmail.com>
2021-06-29 22:00:43 -0500

    product screem

--- _frontend/src/App.js
+++ _frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import data from './data.js';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 function App() {
   return (
+      <BrowserRouter>
+
     <div className="grid-container">
         <header className="row">
 
@@ -16,44 +21,8 @@ function App() {
             </div>
         </header>
         <main>
-            <div className="row center">
-              {
-                data.products.map((product) =>(
-
-                  <div key={product._id} className="card">
-                    <a href={`/product/${product._id}`}>
-                        <img className="medium" src={product.image} alt={product.name} />
-                    </a>
-
-                    <div className="card-body">
-                        <a href={`/product/${product._id}`}>
-                            <h2>{product.name}</h2>
-                        </a>
-                        <div className="rating">
-                            <span><i className="fa fa-star"></i> </span>
-                            <span><i className="fa fa-star"></i> </span>
-                            <span><i className="fa fa-star"></i> </span>
-                            <span><i className="fa fa-star"></i> </span>
-                            <span><i className="fa fa-star-o"></i> </span>
-                        </div>
-                        <div className="price">
-                            $ {product.price}
-                        </div>
-
-
-                    </div>
-                </div>
-                ) )
-              }
-                
-                
-                
-
-
-
-            </div>
-
-
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
 
         </main>
 
@@ -63,6 +32,7 @@ function App() {
         </footer>
 
     </div>
+    </BrowserRouter>
   );
 }
 
